7e2ff20040352f78d7d53324425cc5184115d138
Lee-412 <leeduchht@gmail.com>
2025-04-02 01:24:50 +0700

    fix(permission): fix bug

--- auth/src/main/java/authstream/application/dtos/ApiRoute.java
+++ auth/src/main/java/authstream/application/dtos/ApiRoute.java
@@ -3,11 +3,9 @@ package authstream.application.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class ApiRoute {
     public String path;
-    public List<String> method;
+    public String method;
 }
